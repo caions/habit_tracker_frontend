733ffa62ce9dcdd427005b57626a5dd8d40b84b6
Caio Nascimento <caionsantos2000@gmail.com>
2023-10-20 22:32:39 -0300

    feat: add a habit buttom

--- lib/header.dart
+++ lib/header.dart
@@ -12,9 +12,30 @@ class Header extends StatelessWidget {
           'Good Morning, Caio',
           style: TextStyle(color: Colors.white, fontSize: 50),
         ),
-        const Text(
-          'Fri, Oct 20 - 09',
-          style: TextStyle(color: Colors.white, fontSize: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              'Fri, Oct 20 - 09',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            OutlinedButton(
+              style: ButtonStyle(
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.blue),
+                  overlayColor: MaterialStateProperty.all(Colors.white12)),
+              onPressed: () => {},
+              child: const Row(
+                children: [
+                  Icon(Icons.add, color: Colors.white, size: 18),
+                  SizedBox(width: 3),
+                  Text('Add a Habit'),
+                ],
+              ),
+            )
+          ],
         ),
         const SizedBox(height: 30),
         const LinearProgressIndicator(
